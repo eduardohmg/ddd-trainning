e98faa348abb139282add03bfb22e3ebb95da242
Eduardo Henrique Maia Garcia <eduardohmgarcia@gmail.com>
2024-02-23 23:28:44 +0100

    feat: add interface for domain events and use it in withdrawn event

--- src/main/java/org/garcia/trainning/ddd/account/MoneyWithdrawn.java
+++ src/main/java/org/garcia/trainning/ddd/account/MoneyWithdrawn.java
@@ -1,4 +1,6 @@
 package org.garcia.trainning.ddd.account;
 
-public record MoneyWithdrawn(AccountID accountID, Money amount) {
+import org.garcia.trainning.ddd.archetype.DomainEvent;
+
+public record MoneyWithdrawn(AccountID accountID, Money amount) implements DomainEvent {
 }
---
+++ src/main/java/org/garcia/trainning/ddd/archetype/DomainEvent.java
@@ -0,0 +1,4 @@
+package org.garcia.trainning.ddd.archetype;
+
+public interface DomainEvent {
+}
